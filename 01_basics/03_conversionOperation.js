28f1ab157c4cdddc4947e08d3263cd5e2629c465
Jai Somani <142048378+jaisomani04@users.noreply.github.com>
2024-04-01 12:21:49 +0000

    Committing the datatypes and conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
